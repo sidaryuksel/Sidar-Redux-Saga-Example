f0e9a5bf82aab083dbd5cb8d8176f83c018e91ff
sidartastan <sidartastan@gmail.com>
2019-10-16 12:53:42 +0300

    index test ettiğim şeyi düzellttim

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import store from './reducers/configureStore';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
